4cf9d218db48ca603813c42b9f66fa8546ce3f73
Mohan Talkad Nagaraju <m.nagaraju@devon.nl>
2021-06-04 10:15:43 +0530

    Init-commit-project-setup

---
+++ src/devon.dotnetcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devon.dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/devon.dotnetcore/startup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace devon.dotnetcore
+{
+    class startup
+    {
+    }
+}
